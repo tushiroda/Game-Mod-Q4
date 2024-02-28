1108320fbfb80bb0e8cabcf99e8ac6b36c107bbc
tushiroda <taeshinushiroda@gmail.com>
2024-02-28 17:35:36 -0500

    hyperblaster -> volt, machinegun -> r-97

--- game/weapon/WeaponMachinegun.cpp
+++ game/weapon/WeaponMachinegun.cpp
@@ -226,19 +226,14 @@ stateResult_t rvWeaponMachinegun::State_Fire ( const stateParms_t& parms ) {
 	};	
 	switch ( parms.stage ) {
 		case STAGE_INIT:
-			if ( wsfl.zoom ) {
-				nextAttackTime = gameLocal.time + (altFireRate * owner->PowerUpModifier ( PMOD_FIRERATE ));
-				Attack ( true, 1, spreadZoom, 0, 8.0f );
-				fireHeld = true;
-			} else {
-				nextAttackTime = gameLocal.time + (fireRate * owner->PowerUpModifier ( PMOD_FIRERATE ));
-				Attack ( false, 1, 0.1, 0, 8.0f );
-			}
+			nextAttackTime = gameLocal.time + (fireRate * owner->PowerUpModifier ( PMOD_FIRERATE ));
+			Attack ( false, 1, spread, 0, 1.0f );
+			
 			PlayAnim ( ANIMCHANNEL_ALL, "fire", 0 );
 			return SRESULT_STAGE ( STAGE_WAIT );
 	
 		case STAGE_WAIT:		
-			if ( fireHeld && wsfl.attack && gameLocal.time >= nextAttackTime && AmmoInClip() && !wsfl.lowerWeapon ) {
+			if ( !fireHeld && wsfl.attack && gameLocal.time >= nextAttackTime && AmmoInClip() && !wsfl.lowerWeapon ) {
 				SetState ( "Fire", 0 );
 				return SRESULT_DONE;
 			}
